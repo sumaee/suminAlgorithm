06246ed835cd467434e022fabcdc2f38a51d209d
박수민 <109265165+sumaee@users.noreply.github.com>
2022-08-27 22:04:01 +0900

    [Silver I] Title: 직사각형, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.java
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -1,32 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 4; i++) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int p1 = sc.nextInt();
-			int q1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-			int p2 = sc.nextInt();
-			int q2 = sc.nextInt();
-			
-			if (p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1) {
-				System.out.println("d");
+		for (int tc = 0; tc < 4; tc++) {
+			st = new StringTokenizer(br.readLine());
+
+			// 첫번째 직사각형 좌표
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int p1 = Integer.parseInt(st.nextToken());
+			int q1 = Integer.parseInt(st.nextToken());
+
+			// 두번째 직사각형 좌표
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int p2 = Integer.parseInt(st.nextToken());
+			int q2 = Integer.parseInt(st.nextToken());
+
+			// 점일 경우
+			if ((x1 == p2 && y1 == q2) || (x1 == p2 && q1 == y2) || (p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2)) {
+				sb.append("c").append("\n");
 			}
-			else if ((x1 == p2 && y1 == q2) || (x1 == p2 && q1 == y2) || (p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2)) {
-				System.out.println("c");
+			// 공통 부분이 없는 경우
+			else if (p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1) {
+				sb.append("d").append("\n");
 			}
-			else if (p1 == x2 || q1 == y2|| p2 == x1 || y1 == q2){
-				System.out.println("b");
+			// 선분으로 겹치는 경우
+			else if (p1 == x2 || q1 == y2|| p2 == x1 || y1 == q2) {
+				sb.append("b").append("\n");
 			}
+			// 위의 어떠한 경우도 아니라면 공통부분 없음
 			else {
-				System.out.println("a");
+				sb.append("a").append("\n");
 			}
-		}
-	}
 
+		} // tc
+		System.out.println(sb);
+	
+    
+	}// main
 }
