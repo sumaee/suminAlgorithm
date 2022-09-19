6f36096c0148ee862e3a864d18ddb6e9516944f8
박수민 <109265165+sumaee@users.noreply.github.com>
2022-09-19 19:08:38 +0900

    [D3] Title: 햄버거 다이어트, Time: 737 ms, Memory: 20,884 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,63 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, L;
-	static int[] taste;
-	static int[] K;
-	static boolean[] check;// 골랐는지 아닌지 확인
-	static int ans;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int test_case = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
+		for (int tc = 1; tc <= test_case; tc++) {
+			sb.append("#" + tc + " ");
+
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken()); // 재료 수
+			int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
 
-			taste = new int[N];
-			K = new int[N];
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				taste[i] = Integer.parseInt(st.nextToken());
-				K[i] = Integer.parseInt(st.nextToken());
+			int[] score = new int[N];
+			int[] cal=new int[N];
+			int result=Integer.MIN_VALUE;
+			
+			for(int i=0; i<N; i++) {
+				st=new StringTokenizer(br.readLine());
+				score[i]=Integer.parseInt(st.nextToken());
+				cal[i]=Integer.parseInt(st.nextToken());
 			}
-			check = new boolean[N];
-			powerSet(0);
-			sb.append(ans).append("\n");
-			ans = 0;
-
-		} // tc
-		System.out.println(sb);
-	}// main
-
-	static void powerSet(int idx) {
-		// 재료의 갯수인 N까지 idx가 왔다면 모든 재료를 구한것
-		if (idx == N) {
-			// 고른 재료들의 맛의 합과 칼로리의 합을 수함
-			// 더한 칼로리의 합이 K가 넘지 않는다면
-			// 현재 맛 점수와 ans 중 큰값을 구함
-			int tSum = 0, kSum = 0;
-			for (int i = 0; i < N; i++) {
-				// i번째 재료
-				if (check[i]) {
-					tSum += taste[i];
-					kSum += K[i];
+			
+			for(int i=0; i<(1<<N); i++) {
+				int scoreSum=0;
+				int calSum=0;
+				for(int j=0; j<N; j++) {
+					if((i & (1<<j))>0) {
+						calSum+=cal[j];
+						scoreSum+=score[j];
+					}
+				}
+				if(calSum<L) {
+					result=Math.max(scoreSum, result);
 				}
 			}
-			if (kSum <= L) {
-				ans = Math.max(ans, tSum);
-			}
-			return;
-		}
-		check[idx] = true; // 재료 고름
-		powerSet(idx + 1);
-		check[idx] = false; // 재료 안고름
-		powerSet(idx + 1);
-	}
+			
+			sb.append(result).append("\n");
+			
+		}//tc
+		System.out.println(sb);
+	}//main
 }
