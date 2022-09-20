d14fa367cef0bbe154dd43076b37c128a2a858b1
박수민 <109265165+sumaee@users.noreply.github.com>
2022-09-20 21:27:11 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,391 ms, Memory: 25,228 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,48 +2,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int test_case = Integer.parseInt(br.readLine());
-
-		for (int tc = 1; tc <= test_case; tc++) {
-			sb.append("#" + tc + " ");
-
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // 재료 수
-			int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
-
-			int[] score = new int[N];
-			int[] cal=new int[N];
-			int result=Integer.MIN_VALUE;
-			
-			for(int i=0; i<N; i++) {
-				st=new StringTokenizer(br.readLine());
-				score[i]=Integer.parseInt(st.nextToken());
-				cal[i]=Integer.parseInt(st.nextToken());
-			}
-			
-			for(int i=0; i<(1<<N); i++) {
-				int scoreSum=0;
-				int calSum=0;
-				for(int j=0; j<N; j++) {
-					if((i & (1<<j))>0) {
-						calSum+=cal[j];
-						scoreSum+=score[j];
-					}
-				}
-				if(calSum<L) {
-					result=Math.max(scoreSum, result);
-				}
-			}
-			
-			sb.append(result).append("\n");
-			
-		}//tc
-		System.out.println(sb);
-	}//main
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int test_case = Integer.parseInt(br.readLine());
+ 
+        for (int tc = 1; tc <= test_case; tc++) {
+            sb.append("#" + tc + " ");
+ 
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); // 재료 수
+            int L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+ 
+            int[] score = new int[N];
+            int[] cal=new int[N];
+            int result=Integer.MIN_VALUE;
+             
+            for(int i=0; i<N; i++) {
+                st=new StringTokenizer(br.readLine());
+                score[i]=Integer.parseInt(st.nextToken());
+                cal[i]=Integer.parseInt(st.nextToken());
+            }
+             
+            for(int i=0; i<(1<<N); i++) {
+                int scoreSum=0;
+                int calSum=0;
+                for(int j=0; j<N; j++) {
+                    if((i & (1<<j))>0) {
+                        calSum+=cal[j];
+                        scoreSum+=score[j];
+                    }
+                }
+                if(calSum<L) {
+                    result=Math.max(scoreSum, result);
+                }
+            }
+             
+            sb.append(result).append("\n");
+             
+        }//tc
+        System.out.println(sb);
+    }//main
 }
