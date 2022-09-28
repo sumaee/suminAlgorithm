55eacfb0d9c137ce9173d69755a19cd1955a162a
박수민 <109265165+sumaee@users.noreply.github.com>
2022-09-28 19:00:09 +0900

    [D3] Title: 장애물 경주 난이도, Time: 114 ms, Memory: 30,376 KB -BaekjoonHub

--- SWEA/D3/6730. 장애물 경주 난이도/장애물 경주 난이도.java
+++ SWEA/D3/6730. 장애물 경주 난이도/장애물 경주 난이도.java
@@ -17,8 +17,8 @@ public class Solution {
 			sb.append("#" + tc + " ");
 			N = Integer.parseInt(br.readLine()); // 블록 개수
 			block = new int[N];
-			up = Integer.MIN_VALUE; // 올라갈 때 최고 변화
-			down = Integer.MIN_VALUE; // 내렬갈 때 최고 변화
+			up = 0; // 올라갈 때 최고 변화
+			down = 0; // 내렬갈 때 최고 변화
 
 			// 블록을 먼저 채워주기
 			st = new StringTokenizer(br.readLine());
@@ -27,28 +27,16 @@ public class Solution {
 			}
 
 			for (int i = 0; i < N - 1; i++) {
-				check(block[i], block[i + 1]);
+				int temp = Math.abs(block[i + 1] - block[i]);
+				if (block[i] > block[i + 1]) {
+					down = Math.max(down, temp);
+				} else { // 올라가는 것
+					up = Math.max(up, temp);
+				}
 			}
-			
-			if (up == Integer.MIN_VALUE) {
-				up = 0;
-			}
-			
-			if (down == Integer.MIN_VALUE) {
-				down = 0;
-			}
+
 			sb.append(up + " " + down).append("\n");
 		} // tc
 		System.out.println(sb);
 	}// main
-
-	private static void check(int a, int b) {
-
-		// 내려가는 것
-		if (a > b) {
-			down = Math.max(down, a - b);
-		} else { // 올라가는 것
-			up = Math.max(up, b - a);
-		}
-	}
 }
