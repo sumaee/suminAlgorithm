b2024358f290b8c93baa1fa7c8a848d7bfd28b26
박수민 <109265165+sumaee@users.noreply.github.com>
2022-10-04 23:26:34 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 84 ms, Memory: 11820 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		int[] LIS = new int[N];
+		int[] tmp = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 우선 첫번째 값을 그냥 넣기
+		LIS[0] = arr[0];
+		tmp[0] = 1;
+		int LISLen = 1;
+
+		// 이 다음부터 나오는 수가 집어넣은 마지막 수보다 크다면 추가
+		// 작다면 집어넣을 곳을 탐색하여 대치
+		for (int i = 1; i < N; i++) {
+
+			int value = arr[i];
+
+			// 만약 넣으려는 수가 LIS에 들어가있는 마지막 수보다 크면 그냥 뒤에 추가
+			if (LIS[LISLen - 1] < value) {
+				LIS[LISLen] = value;
+				LISLen++;
+				tmp[i] = LISLen;
+			}
+
+			// 아니라면 들어갈곳 찾아서 대치
+			else {
+				int start = 0;
+				int end = LISLen;
+
+				while (start < end) {
+					int mid = (start + end) / 2;
+
+					if (LIS[mid] < value) {
+						start = mid + 1;
+					} else {
+						end = mid;
+					}
+
+				}
+
+				LIS[start] = value;
+				tmp[i] = start + 1;
+			}
+		}
+
+		sb.append(LISLen + "\n");
+
+		int[] result = new int[LISLen];
+		for (int i = tmp.length - 1; i >= 0; i--) {
+			if (tmp[i] == LISLen) {
+				result[LISLen - 1] = arr[i];
+				LISLen--;
+			}
+		}
+
+		for (int i : result) {
+			sb.append(i + " ");
+		}
+		System.out.println(sb);
+	}// main
+}
