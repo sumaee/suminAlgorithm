440698ee38ac0961bb3373113e4a29ca6441fb4b
박수민 <109265165+sumaee@users.noreply.github.com>
2022-11-01 17:39:30 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; n-(i*(i-1)/2) > 0 ; i++){
+            if((n-(i*(i-1)/2))%i == 0){
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+}
