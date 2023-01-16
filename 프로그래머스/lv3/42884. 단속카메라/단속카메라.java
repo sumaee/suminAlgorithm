b9032fb22882f14705a1f4486114ed313b923b75
박수민 <109265165+sumaee@users.noreply.github.com>
2023-01-17 08:51:35 +0900

    [level 3] Title: 단속카메라, Time: 6.83 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.java
@@ -2,26 +2,19 @@ import java.util.*;
 import java.lang.*;
 
 class Solution {
-    // 고속도로에 먼저 진입한 순서로 정렬 후 진입 차로에 설치
+    // 고속도로를 먼저 진출한 순서로 정렬 후 진출 차로에 설치
     public int solution(int[][] routes) {
         int answer = 0;
-        List<int[]> car = new ArrayList<>();
-        for(int[] route: routes){
-           car.add(route);
-        }
-        Collections.sort(car, (a,b) -> b[0] - a[0]);
-        while(!car.isEmpty()) {
-            int position = car.get(0)[0];
- 
-            for(int i = 0; i<car.size(); i++ ) {
-                if(car.get(i)[1]>=position){
-                    car.remove(i);
-                    i--;
-                } 
+     
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
+           
+        int camera = Integer.MIN_VALUE;
+        for(int[] route : routes){            
+            if(route[0]>camera){
+                answer++;
+                camera = route[1];
             }
-
-            answer++;
         }
         return answer;
-}
+    }
 }
