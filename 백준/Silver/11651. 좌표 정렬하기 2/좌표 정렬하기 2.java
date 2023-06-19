832cb57eee23e123541a229f24e487bf1809ce63
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-19 17:33:28 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1788 ms, Memory: 57100 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,48 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class a {
-	int x;
-	int y;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public a(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+        int n = Integer.parseInt(br.readLine());
+        List<Pair2> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(new Pair2(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        list.sort((o1, o2) -> o1.y == o2.y ? o1.x - o2.x : o1.y - o2.y);
+
+        for (Pair2 pair : list) {
+            System.out.println(pair.x + " " + pair.y);
+        }
+    }
 }
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+class Pair2 {
+    int x, y;
 
-		int N = Integer.parseInt(br.readLine());
-		List<a> arr = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				arr.add(new a(x, y));
-		}
-
-		Collections.sort(arr, new Comparator<a>() {
-			@Override
-			public int compare(a o1, a o2) {
-				if (o1.y == o2.y) {
-					return o1.x - o2.x;
-				} else {
-					return o1.y - o2.y;
-				}
-			}
-		});
-
-		for (int i = 0; i < N; i++) {
-			System.out.println(arr.get(i).x + " " + arr.get(i).y);
-		}
-	}
+    public Pair2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
