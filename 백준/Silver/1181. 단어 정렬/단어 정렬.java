84f981cb349950724160639fce0b12b530decf14
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-19 17:13:13 +0900

    [Silver V] Title: 단어 정렬, Time: 600 ms, Memory: 25608 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,47 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		List<String> arr = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
 
-		//입력을 받을 때부터 중복 없애기
-		next: for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			if (i == 0) {
-				arr.add(str);
-				continue;
-			}
-			for (int j = 0; j < arr.size(); j++) {
-				if (str.equals(arr.get(j))) {
-					continue next;
-				}
-			}
-			arr.add(str);
-		}
+        //중복 확인할 set
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
 
-		Collections.sort(arr, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				if (o1.length() == o2.length()) {
-					return o1.compareTo(o2);
-				} else {
-					return o1.length() - o2.length();
-				}
-			}
-		});
+        List<String> list = new ArrayList<>(set);
 
-		for (int i = 0; i < arr.size(); i++) {
-			System.out.println(arr.get(i));
-		}
+        Collections.sort(list, new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
 
-	}
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
+
 }
