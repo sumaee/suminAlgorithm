4bfacef55a7043cf1d23d72ea2cfe68370b2b86f
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-21 21:00:36 +0900

    [Gold IV] Title: N-Queen, Time: 7816 ms, Memory: 18548 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static boolean[][] board;
+    static int answer;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        board = new boolean[n][n];
+        answer = 0;
+
+        chess(0);
+        System.out.println(answer);
+
+    }
+
+    private static void chess(int row) {
+        if (row == n) {
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            //퀸을 놓을 수 있는 자리인지 아닌지 확인 true: 놓을수 없음/ false: 놓을수 있음
+            if (isPossible(row, i)) {
+                continue;
+            }
+
+            board[row][i] = true;
+            chess(row + 1);
+            board[row][i] = false;
+        }
+    }
+
+    private static boolean isPossible(int row, int col) {
+        //해당 위치에서 위의 방향에 퀸이 존재하는지 확인
+        for (int i = row; i >= 0; i--) {
+            //존재한다면 true반환
+            if (board[i][col]) {
+                return true;
+            }
+        }
+
+        //오른쪽 대각선 위로 확인
+        int dc = col + 1;
+        int dr = row - 1;
+        while (dr >= 0 && dc < n) {
+            if (board[dr--][dc++]) return true;
+        }
+
+        //왼쪽 대각선 위로 확인
+        dc = col - 1;
+        dr = row - 1;
+        while (dr >= 0 && dc >= 0) {
+            if (board[dr--][dc--]) return true;
+        }
+
+        return false;
+    }
+}
