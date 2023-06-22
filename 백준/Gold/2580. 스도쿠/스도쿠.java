e5e5a0100927fe32871e795aacad7aea4bce8473
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-22 16:00:15 +0900

    [Gold IV] Title: 스도쿠, Time: 564 ms, Memory: 24568 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -4,86 +4,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr = new int[9][9];
+    static int[][] board = new int[9][9];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		// 스도쿠 배열 채우기
-		for (int i = 0; i < 9; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        //스도쿠 채우기
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		check(0, 0);
-	}// main
+        findAnswer(0, 0);
+    }
 
-	public static void check(int row, int col) {
-		// 만약 한줄을 다 돌렸다면 다음 줄로 넘기기
-		if (col == 9) {
-			check(row + 1, 0);
-			return;
-		}
-		
-		// 만약 마지막 줄까지 다 갔다면 될수 있는 수를 다 채우고 왔다는 것이므로 출력
-		if (row == 9) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					sb.append(arr[i][j] + " ");
-				}
-				sb.append("\n");
-			}
-			System.out.println(sb);
-			System.exit(0);
-		}
-		
-		// 해당 수가 0이라면 채워넣고 가능한지 확인 후 다음단계 넘어가든 말든 정하기
-		if (arr[row][col] == 0) {
-			for (int k = 1; k <= 9; k++) {
-				if (isPossible(row, col, k)) {
-					arr[row][col] = k;
+    private static void findAnswer(int row, int col) {
+        //한줄을 다 채웠으면 다음
+        if (col == 9) {
+            findAnswer(row + 1, 0);
+            return;
+        }
 
-					// 다음 넘어가서 빈칸인지 확인
-					check(row, col + 1);
-				}
-			}
+        //스도쿠가 다 채워지면 출력하고 끝내버리기
+        if (row == 9) {
+            StringBuilder sb = new StringBuilder();
+            for (int[] r : board) {
+                for (int c : r) {
+                    sb.append(c).append(" ");
+                }
+                sb.append("\n");
+            }
 
-			// 만약 9까지 다 돌았는데 들어가는게 없으면
-			// 안되는 것이므로 그냥 0으로 바꾸고 끝내기
-			arr[row][col] = 0;
-			return;
-		}
+            System.out.println(sb);
+            System.exit(0);
+        }
 
-		// 빈칸이 아니면 그냥 넘기기
-		check(row, col + 1);
-	}
+        //빈칸이라면 확인
+        if (board[row][col] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                if (isPossible(row, col, i)) {
+                    board[row][col] = i;
+                    findAnswer(row, col + 1);
+                }
+            }
 
-	public static boolean isPossible(int row, int col, int num) {
-		// 스도쿠를 돌며 해당 숫자가 없으면 가능한 걸로 판단
-		// 가로, 세로 검사
-		for (int i = 0; i < 9; i++) {
+            //포문에 안걸리고 여기 왔다는 것은 되는 숫자가 없다는 것이므로 0으로 바꾸고 끝내기
+            board[row][col] = 0;
+            return;
+        }
+        findAnswer(row, col + 1);
+    }
 
-			if (arr[i][col] == num || arr[row][i] == num) {
-				return false;
-			}
-		}
-
-		// 사각형 검사
-		// r, c : 빈칸이 있는 곳의 가장 왼쪽 위 지점
-
-		int r = row / 3 * 3;
-		int c = col / 3 * 3;
-		for (int i = r; i < r + 3; i++) {
-			for (int j = c; j < c + 3; j++) {
-				if (arr[i][j] == num) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    private static boolean isPossible(int row, int col, int num) {
+        //작은 사각형 확인
+        for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++) {
+            for (int j = col / 3 * 3; j < col / 3 * 3 + 3; j++) {
+                if (board[i][j] == num) return false;
+            }
+        }
+        //가로세로줄 확인
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) return false;
+        }
+        return true;
+    }
 }
