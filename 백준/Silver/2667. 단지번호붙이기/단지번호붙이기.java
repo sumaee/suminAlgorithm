1b0062f0dac029f46e38d621558f8e5c9c315048
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-23 13:01:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -6,59 +6,67 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-	static int N;
-	static int[][] arr;
-	static boolean[][] visited;
-	static int result;
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	static List<Integer> list;
+    static int[][] apart;
+    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dc = {0, 0, -1, 1}; // 상하좌우
+    static int count;
+    static int n;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		N = Integer.parseInt(br.readLine()); // 지도 크기
-		arr = new int[N][N];
-		visited = new boolean[N][N];
-		list = new ArrayList<>();
+        n = Integer.parseInt(br.readLine());
+        apart = new int[n][n];
+        visited = new boolean[n][n];
 
-		for (int i = 0; i < N; i++) {
-			String a = br.readLine();
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = a.charAt(j) - '0';
-			}
-		}
-		int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                apart[i][j] = str.charAt(j) - '0';
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (arr[i][j] == 1 && !visited[i][j]) {
-					result = 1;
-					cnt++;
-					dfs(i, j);
-					list.add(result);
-					Collections.sort(list);
-				}
-			}
-		}
-		for (int i : list) {
-			sb.append(i).append("\n");
-		}
-		System.out.println(cnt);
-		System.out.println(sb);
-	}// main
+        int apartCnt = 0;
+        List<Integer> counts = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (apart[i][j] == 1 && !visited[i][j]) {
+                    count = 1;
+                    dfs(i, j);
+                    counts.add(count);
+                    apartCnt++;
+                }
+            }
+        }
 
-	public static void dfs(int row, int col) {
-		visited[row][col] = true;
-		for (int i = 0; i < 4; i++) {
-			int r = row + dr[i];
-			int c = col + dc[i];
+        Collections.sort(counts);
+        sb.append(apartCnt).append("\n");
+        for (int i = 0; i < counts.size(); i++) {
+            sb.append(counts.get(i)).append("\n");
+        }
 
-			if (r >= 0 && c >= 0 && r < N && c < N && arr[r][c] == 1 && !visited[r][c]) {
-				result++;
-				dfs(r, c);
-			}
-		}
-	}// dfs
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int row, int col) {
+        visited[row][col] = true;
+
+        //상하좌우 확인
+        for (int i = 0; i < 4; i++) {
+            int drow = row + dr[i];
+            int dcol = col + dc[i];
+
+            //해당 범위가 aprat 범위를 벗어난다면 다음
+            if (drow < 0 || drow >= n || dcol < 0 || dcol >= n) continue;
+
+            //벗어나지 않고 방문하지 않은 아파트라면 연결된 부분 확인
+            if (apart[drow][dcol] == 1 && !visited[drow][dcol]) {
+                count++;
+                dfs(drow, dcol);
+            }
+        }
+    }
 }
