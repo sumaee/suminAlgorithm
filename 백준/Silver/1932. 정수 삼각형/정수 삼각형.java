31e4492ece163123c4d2bcbe1db59961aa269958
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-23 16:57:42 +0900

    [Silver I] Title: 정수 삼각형, Time: 268 ms, Memory: 26484 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,48 +4,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	// 거꾸로 올라오면서 최댓값과 해당 자리의 합 구해서 check 배열에 채우기
-	static int N;
-	static int[][] arr;
-	static int[][] check;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        int[][] sum = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		check = new int[N][N];
+                //누적합 마지막줄 채우기
+                if (i == n - 1) {
+                    sum[i][j] = arr[i][j];
+                }
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < i + 1; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        //거꾸로 진행
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                sum[row][col] = Math.max(sum[row + 1][col], sum[row + 1][col + 1]) + arr[row][col];
+            }
+        }
 
-		// check 배열 마지막 줄 채우기
-		for (int i = 0; i < N; i++) {
-			check[N - 1][i] = arr[N - 1][i];
-		}
-
-		// 맨 위부터 찾기 시작이지만 사실상 아래서부터 채우기가 됨
-		int result = findMax(0, 0);
-		
-		System.out.println(result);
-
-	}// main
-
-	static int findMax(int row, int col) {
-        //만약 마지막 행이라면 해당 행을 보냄
-		if (row == N - 1) {
-			return check[row][col];
-		}
-		
-		//값이 채워져 있지 않은 경우라면 다음 행의 두개를 비교하여 큰 값과 더한값을 채우기
-		if(check[row][col]==0) {
-			check[row][col]=Math.max(findMax(row+1, col), findMax(row+1, col+1))+arr[row][col];
-		}
-		return check[row][col];
-	}
+        System.out.println(sum[0][0]);
+        
+    }
 }
