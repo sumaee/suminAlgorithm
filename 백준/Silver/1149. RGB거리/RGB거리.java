f4cd6dcfde0de0475f1735092ceee41168f6490c
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-24 16:05:14 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -4,57 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] rgb;
-	static int[][] sum;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N=Integer.parseInt(br.readLine());
-		
-		// rgb[][0] : red
-		// rgb[][1] : green
-		// rgb[][2] : blue
-		rgb=new int[N][3];
-		sum=new int[N][3];
-		
-		for(int i=0; i<N; i++) {
-			st=new StringTokenizer(br.readLine());
-			
-			for(int j=0; j<3; j++) {
-				rgb[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		sum[N-1][0]=rgb[N-1][0];
-		sum[N-1][1]=rgb[N-1][1];
-		sum[N-1][2]=rgb[N-1][2];
-		
-		
-		int result=Math.min(Math.min(findMin(0, 0), findMin(0, 1)), findMin(0, 2));
-		
-		System.out.println(result);
-		
-	}//main
-	
-	static int findMin(int row, int col) {
-		
-		if(row==N-1||sum[row][col]!=0) {
-			return sum[row][col];
-		}
-		
-		if(sum[row][col]==0) {
-			if(col==0) {
-				sum[row][col]=Math.min(findMin(row+1, col+1), findMin(row+1, col+2))+rgb[row][col];
-				
-			}else if(col==1) {
-				sum[row][col]=Math.min(findMin(row+1, col-1), findMin(row+1, col+1))+rgb[row][col];
-				
-			}else if(col==2){
-				sum[row][col]=Math.min(findMin(row+1, col-2), findMin(row+1, col-1))+rgb[row][col];
-			}
-		}
-		return sum[row][col];
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] prices = new int[n][3];
+        int[][] sum = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                prices[i][j] = Integer.parseInt(st.nextToken());
+            }
+
+            if (i == n - 1) sum[i] = prices[i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    sum[i][j] = Math.min(sum[i + 1][j + 1], sum[i + 1][j + 2]) + prices[i][j];
+                } else if (j == 1) {
+                    sum[i][j] = Math.min(sum[i + 1][j - 1], sum[i + 1][j + 1]) + prices[i][j];
+                } else {
+                    sum[i][j] = Math.min(sum[i + 1][j - 2], sum[i + 1][j - 1]) + prices[i][j];
+                }
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            answer = Math.min(answer, sum[0][i]);
+        }
+
+        System.out.println(answer);
+
+    }
 }
