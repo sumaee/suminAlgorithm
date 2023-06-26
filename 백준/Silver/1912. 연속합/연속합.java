34081129b04ad6cc78ba2a71754c88a39505a840
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-26 12:02:17 +0900

    [Silver II] Title: 연속합, Time: 268 ms, Memory: 23860 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] sum = new int[n];
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        sum[0] = arr[0];
+        int answer = sum[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = Math.max(sum[i - 1] + arr[i], arr[i]);
+            answer = Math.max(answer, sum[i]);
+
+        }
+
+        System.out.println(answer);
+
+    }
+}
