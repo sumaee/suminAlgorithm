a39dfb5d920c1b7e13c3f9b012bc7efba609c936
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-29 12:44:32 +0900

    [Gold V] Title: LCS, Time: 168 ms, Memory: 18560 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,30 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		char[] strA = br.readLine().toCharArray();
-		char[] strB = br.readLine().toCharArray();
+        String str1 = br.readLine();
+        String str2 = br.readLine();
 
-		int ALen = strA.length;
-		int BLen = strB.length;
+        //주변을 0으로 채우기 위해 한칸씩 더 크게 만듬
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 
-		int[][] dp = new int[ALen + 1][BLen + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                //만약 두 문자열이 같으면 왼쪽 대각선 위에 있는 값보다 1개 더 크게 저장
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
 
-		for (int i = 1; i <= ALen; i++) {
-			for (int j = 1; j <= BLen; j++) {
+                //다르다면 위랑 왼쪽의 값중 큰 값 저장
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
 
-				if (strA[i - 1] == strB[j - 1]) {
-					dp[i][j] = dp[i - 1][j - 1] + 1;
-				}
-
-				else {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-				}
-			}
-		}
-
-		System.out.println(dp[ALen][BLen]);
-	}// main
+        System.out.println(dp[str1.length()][str2.length()]);
+    }
 }
