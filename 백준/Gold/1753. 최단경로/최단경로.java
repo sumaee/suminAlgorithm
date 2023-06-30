88658078ae8e9d6700e0af04ab6acb806b0f51f8
박수민 <109265165+sumaee@users.noreply.github.com>
2023-06-30 12:54:31 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 117476 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,92 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int V, E, start;
-	static List<Node>[] graph;
-	static boolean[] visited;
-	static int[] sizeSum;
+    static int v, e;
+    static int[] dis;
+    static boolean[] visited;
+    static List<Node>[] node;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		V = Integer.parseInt(st.nextToken()); // 정점의 개수
-		E = Integer.parseInt(st.nextToken()); // 간선의 개수
-		start = Integer.parseInt(br.readLine());// 시작점
-		graph = new ArrayList[V + 1];
-		visited = new boolean[V + 1];
-		sizeSum = new int[V + 1];
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= V; i++) {
-			graph[i] = new ArrayList<>();
-			sizeSum[i] = Integer.MAX_VALUE;
-		}
+        dis = new int[v + 1];
+        Arrays.fill(dis, 1234567890);
+        visited = new boolean[v + 1];
 
-		for (int i = 1; i <= E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int stIdx = Integer.parseInt(st.nextToken());
-			int edIdx = Integer.parseInt(st.nextToken());
-			int size = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+        //초기 설정
+        node = new List[v + 1];
+        for (int i = 1; i <= v; i++) {
+            node[i] = new ArrayList<>();
+        }
 
-			graph[stIdx].add(new Node(edIdx, size));
-		}
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-		dijkstra(start);
+            node[x].add(new Node(y, w));
+        }
 
-		for (int i = 1; i <= V; i++) {
+        dijkstra(start);
 
-			System.out.println(sizeSum[i] == Integer.MAX_VALUE ? "INF" : sizeSum[i]);
-		}
-	}// main
+        for (int i = 1; i <= v; i++) {
+            if (dis[i] == 1234567890) {
+                sb.append("INF").append("\n");
+            } else {
+                sb.append(dis[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> que = new PriorityQueue<>(new Comparator<Node>() {
+    private static void dijkstra(int start) {
+        //가중치 기준으로 오름차순으로 뽑기
+        PriorityQueue<Node> que = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
 
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o1.size - o2.size;
-			}
+        que.offer(new Node(start, 0));
+        dis[start] = 0;
 
-		});
-		que.add(new Node(start, 0));
-		sizeSum[start] = 0;
+        while (!que.isEmpty()) {
+            Node curr = que.poll();
 
-		while (!que.isEmpty()) {
-			Node curr = que.poll();
+            if (visited[curr.idx]) continue;
 
-			if (visited[curr.idx])
-				continue;
+            visited[curr.idx] = true;
 
-			visited[curr.idx] = true;
+            //현재 연결된 부분 확인
+            for (Node nxt : node[curr.idx]) {
+                //만약 방문을 했다면 패스
+                if (visited[nxt.idx]) continue;
 
-			for (Node next : graph[curr.idx]) {
-				if (!visited[next.idx]) {
+                //nxt 에 저장된 거리 VS 현재 거리에서 가중치만큼 더한 거리를 확인해 갱신하고 큐에 넣어줌
+                dis[nxt.idx] = Math.min(dis[nxt.idx], dis[curr.idx] + nxt.w);
+                que.offer(new Node(nxt.idx, dis[nxt.idx]));
+            }
+        }
+    }
+}
 
-					if (sizeSum[next.idx] > sizeSum[curr.idx] + next.size) {
-						sizeSum[next.idx] = sizeSum[curr.idx] + next.size;
-						que.add(new Node(next.idx, sizeSum[next.idx]));
-					}
-				}
-			}
-		}
+class Node {
+    int idx, w;
 
-	}
-
-	private static class Node {
-		int idx;
-		int size;
-
-		public Node(int idx, int size) {
-			this.idx = idx;
-			this.size = size;
-		}
-	}
+    public Node(int idx, int w) {
+        this.idx = idx;
+        this.w = w;
+    }
 }
