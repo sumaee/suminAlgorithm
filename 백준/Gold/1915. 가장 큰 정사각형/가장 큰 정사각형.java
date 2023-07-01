ff428e17c87bc9da89806bbc5fdd9190f1090151
박수민 <109265165+sumaee@users.noreply.github.com>
2023-07-01 13:49:39 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 960 ms, Memory: 107032 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -4,37 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-		int[][] arr = new int[N + 1][M + 1];
+        int[][] arr = new int[n + 1][m + 1];
 
-		for (int i = 1; i <= N; i++) {
-			String[] input = br.readLine().split("");
-			for (int j = 1; j <= M; j++) {
-				arr[i][j] = Integer.parseInt(input[j - 1]);
-			}
-		}
+        for (int i = 1; i <= n; i++) {
+            String[] input = br.readLine().split("");
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(input[j - 1]);
+            }
+        }
 
-		int[][] sum = new int[N + 1][M + 1];
-		int max=0;
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= M; j++) {
-				if (arr[i][j] == 0) {
-					sum[i][j] = 0;
-				} else {
-					sum[i][j] = Math.min(Math.min(sum[i - 1][j], sum[i][j - 1]), sum[i - 1][j - 1]) + 1;
-				}
-				
-				max=Math.max(max, sum[i][j]);
-			}
-		}
-		
-		System.out.println(max*max);
+        int[][] sum = new int[n + 1][m + 1];
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (arr[i][j] == 1) {
+                    sum[i][j] = Math.min(Math.min(sum[i - 1][j], sum[i][j - 1]), sum[i - 1][j - 1]) + 1;
+                    max = Math.max(max, sum[i][j]);
+                }
+            }
+        }
 
-	}
+        System.out.println(max * max);
+
+    }
 }
