fb3dc63159fcb9db9c8659531b66f17f90b7c6e4
박수민 <109265165+sumaee@users.noreply.github.com>
2023-08-23 16:50:38 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 124 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static char[][] boards;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        boards = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                boards[i][j] = input.charAt(j);
+            }
+        }
+
+        int blackStart = checkSum('B');
+        int whiteStart = checkSum('W');
+
+        System.out.println(Math.min(blackStart, whiteStart));
+    }
+
+    private static int checkSum(char startColor) {
+        int[][] init = new int[n][m];
+        int[][] sum = new int[n + 1][m + 1];
+        //시작 색깔과 같은지 다른지 체스판 규칙대로 1(다름), 0(같음) 채우기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i + j) % 2 == 0) {
+                    init[i][j] = boards[i][j] == startColor ? 0 : 1;
+                } else {
+                    init[i][j] = boards[i][j] == startColor ? 1 : 0;
+                }
+            }
+        }
+
+        //누적합 구하기
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] + init[i - 1][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+
+        //k만큼의 구간에서 최솟값 구하기
+        int min = Integer.MAX_VALUE;
+        for (int i = 8; i <= n; i++) {
+            for (int j = 8; j <= m; j++) {
+                int total = sum[i][j] - sum[i - 8][j] - sum[i][j - 8] + sum[i - 8][j - 8];
+
+                min = Math.min(min, total);
+            }
+        }
+        return min;
+    }
+}
