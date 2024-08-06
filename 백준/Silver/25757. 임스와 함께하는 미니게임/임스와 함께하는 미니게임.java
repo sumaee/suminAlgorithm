b169a7c3c314c25148f0b50ac15a2fc3973d3229
박수민 <109265165+sumaee@users.noreply.github.com>
2024-08-06 14:34:00 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 260 ms, Memory: 26928 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        if (game.equals("Y")) {
+            System.out.println(set.size());
+        } else if (game.equals("F")) {
+            System.out.println(set.size() / 2);
+        } else {
+            System.out.println(set.size() / 3);
+        }
+    }
+}
