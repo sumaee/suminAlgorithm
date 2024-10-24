a2027cded895c49c4d15c59c8012d0988af330d5
박수민 <109265165+sumaee@users.noreply.github.com>
2024-10-24 17:56:16 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
