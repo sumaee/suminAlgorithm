853119af382297933c416c2863c5d8d24d169aad
박수민 <109265165+sumaee@users.noreply.github.com>
2024-10-29 21:23:40 +0900

    [Gold II] Title: 저울, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] weights = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            weights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(weights);
+        int sum = 1;
+        for (int w : weights) {
+            if(sum<w) break;
+            sum += w;
+        }
+
+        System.out.println(sum);
+    }
+}
