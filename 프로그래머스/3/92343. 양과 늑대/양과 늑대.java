822bfd9967972146729b679cda2bcc2b8f6f5940
sumaee <109265165+sumaee@users.noreply.github.com>
2025-03-27 15:17:23 +0900

    [level 3] Title: 양과 늑대, Time: 5.55 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 class Solution {
     static int answer;
-    
     public int solution(int[] info, int[][] edges) {
         boolean[] visited = new boolean[info.length];
         int sheepCnt = 0;
@@ -15,7 +14,7 @@ class Solution {
     
     private static void dfs(int idx, int[] info, int[][] edges, boolean[] visited, int sheepCnt, int wolfCnt) {
         visited[idx] = true;
-
+        //해당 위치가 양이라면 answer 갱신
         if (info[idx] == 0) {
             sheepCnt++;
             answer = Math.max(answer, sheepCnt);
@@ -23,17 +22,20 @@ class Solution {
             wolfCnt++;
         }
 
+        //지금까지 모은 양의 수보다 늑대의 수가 많다면 끝냄
         if (wolfCnt >= sheepCnt) {
             return;
         }
 
+        //연결된 곳 돌기
         for (int[] edge : edges) {
+            
+            //부모 노드는 방문했고 자식노드를 방문 안한경우 돌기
             if (visited[edge[0]] && !visited[edge[1]]) {
                 boolean[] tempVisited = new boolean[info.length];
                 for (int i = 0; i < visited.length; i++) {
                     tempVisited[i] = visited[i];
                 }
-
                 dfs(edge[1], info, edges, tempVisited, sheepCnt, wolfCnt);
             }
         }
