cbf21a3084202018c3028784479da74c05a5d8aa
sumaee <109265165+sumaee@users.noreply.github.com>
2025-04-01 21:08:08 +0900

    [level 2] Title: 리코쳇 로봇, Time: 8.13 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,78 +2,60 @@ import java.lang.*;
 import java.util.*;
 
 class Solution {
-    
     static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+    static int[] dc = {0, 1, 0 ,-1};
     public int solution(String[] board) {
-        Node start = new Node();
-        Node end = new Node();
-        char[][] map = new char[board.length][board[0].length()];
-        for (int i = 0; i < map.length; i++) {
-            String str = board[i];
-            for (int j = 0; j < map[i].length; j++) {
-                map[i][j] = str.charAt(j);
-
-                if (map[i][j] == 'R') {
-                    start.row = i;
-                    start.col = j;
-                }
-
-                if (map[i][j] == 'G') {
-                    end.row = i;
-                    end.col = j;
-                }
+        
+        Locate start = null;
+        Locate end = null;
+        for(int i=0; i<board.length ; i++){
+            for(int j=0 ; j<board[i].length() ; j++){
+                if(board[i].charAt(j) == 'R') start = new Locate(i, j, 0);
+                else if(board[i].charAt(j) == 'G') end = new Locate(i, j, 0);
             }
         }
-
-        return bfs(start, end, map);
+        
+        return bfs(start, end, board);
     }
     
-    private static int bfs(Node start, Node end, char[][] map) {
-        Queue<Node> que = new LinkedList<>();
-        boolean[][] visited = new boolean[map.length][map[0].length];
-
-        que.offer(new Node(start.row, start.col, 0));
+    private static int bfs(Locate start, Locate end, String[] board){
+        Queue<Locate> que = new LinkedList<>();
+        boolean[][] visited = new boolean[board.length][board[0].length()];
+        
+        que.offer(start);
         visited[start.row][start.col] = true;
-        while (!que.isEmpty()) {
-            Node curr = que.poll();
-
-
-            //현재 위치에서 상하좌우로 미끄러져 벽이나 장애물이 있는 곳까지 가기
-            for (int i = 0; i < 4; i++) {
+        
+        while(!que.isEmpty()){
+            Locate curr = que.poll();
+            
+            if(curr.row == end.row && curr.col ==end.col) return curr.move;
+            
+            for(int i=0; i<4 ;i++){
                 int drow = curr.row;
                 int dcol = curr.col;
-
-                //도착 위치라면 끝
-                if (curr.row == end.row && curr.col == end.col) {
-                    return curr.cnt;
-                }
-
-                while (drow + dr[i] >= 0 && dcol + dc[i] >= 0 && drow + dr[i] < map.length && dcol + dc[i] < map[0].length && map[drow + dr[i]][dcol + dc[i]] != 'D') {
+                
+                while(drow+dr[i] >=0 && dcol + dc[i] >=0 && drow + dr[i] < board.length && dcol+dc[i]<board[0].length()
+                     && board[drow+dr[i]].charAt(dcol + dc[i]) != 'D'){
                     drow += dr[i];
                     dcol += dc[i];
                 }
-
-                if (visited[drow][dcol]) continue;
+                
+                if(visited[drow][dcol]) continue;
                 visited[drow][dcol] = true;
-                que.offer(new Node(drow, dcol, curr.cnt + 1));
+                que.offer(new Locate(drow, dcol, curr.move+1));
             }
         }
-
+        
         return -1;
     }
     
-    static class Node {
-        int row, col, cnt;
-
-        public Node(int row, int col, int cnt) {
+    static class Locate{
+        int row, col, move;
+        
+        public Locate(int row, int col, int move){
             this.row = row;
             this.col = col;
-            this.cnt = cnt;
-        }
-
-        public Node() {
+            this.move = move;
         }
     }
-
 }
