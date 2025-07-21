be6833188a1ac80f5e5b422b303d09b117aacb4d
sumaee <109265165+sumaee@users.noreply.github.com>
2025-07-21 16:10:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int) br.readLine().charAt(0));
+    }
+}
