b86666cb5c6db5e91c3de94c85bc6d658bf6cf00
sumaee <109265165+sumaee@users.noreply.github.com>
2025-10-03 10:47:01 +0900

    [Gold III] Title: 불!, Time: 552 ms, Memory: 72060 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int[] dr = {-1, 0, 1, 0};
@@ -22,18 +19,15 @@ public class Main {
         map = new Character[r][c];
         fire = new LinkedList<>();
         jh = new LinkedList<>();
-
         for (int i = 0; i < r; i++) {
             String str = br.readLine();
             for (int j = 0; j < c; j++) {
                 map[i][j] = str.charAt(j);
-
-                if (map[i][j] == 'F') {
-                    fire.offer(new Node(i, j));
-                } else if (map[i][j] == 'J') {
+                if (map[i][j] == 'J') {
                     jh.offer(new Node(i, j, 0));
+                } else if (map[i][j] == 'F') {
+                    fire.offer(new Node(i, j));
                 }
-
             }
         }
 
@@ -53,10 +47,11 @@ public class Main {
                 Node currFire = fire.poll();
 
                 for (int j = 0; j < 4; j++) {
-                    int drow = currFire.row + dr[j];
-                    int dcol = currFire.col + dc[j];
+                    int drow = dr[j] + currFire.r;
+                    int dcol = dc[j] + currFire.c;
 
-                    if (!isPossible(drow, dcol) || map[drow][dcol] == 'F' || map[drow][dcol] == '#') continue;
+                    if (drow < 0 || dcol < 0 || drow >= r || dcol >= c
+                            || map[drow][dcol] == 'F' || map[drow][dcol] == '#') continue;
                     map[drow][dcol] = 'F';
                     fire.offer(new Node(drow, dcol));
                 }
@@ -64,42 +59,37 @@ public class Main {
 
             int jhCnt = jh.size();
             for (int i = 0; i < jhCnt; i++) {
-                Node currjh = jh.poll();
+                Node currJh = jh.poll();
                 for (int j = 0; j < 4; j++) {
-                    int drow = currjh.row + dr[j];
-                    int dcol = currjh.col + dc[j];
+                    int drow = dr[j] + currJh.r;
+                    int dcol = dc[j] + currJh.c;
 
-                    //범위를 벗어났다는 탈출 성공
-                    if (!isPossible(drow, dcol)) {
-                        answer = currjh.cnt + 1;
+                    if (drow < 0 || dcol < 0 || drow >= r || dcol >= c) {
+                        answer = currJh.cnt + 1;
                         return true;
                     }
 
                     if (map[drow][dcol] == 'F' || map[drow][dcol] == '#' || map[drow][dcol] == 'J') continue;
                     map[drow][dcol] = 'J';
-                    jh.offer(new Node(drow, dcol, currjh.cnt + 1));
+                    jh.offer(new Node(drow, dcol, currJh.cnt + 1));
                 }
             }
         }
-
         return false;
     }
 
-    private static boolean isPossible(int drow, int dcol) {
-        return drow >= 0 && dcol >= 0 && drow < r && dcol < c;
-    }
 
     static class Node {
-        int row, col, cnt;
+        int r, c, cnt;
 
-        public Node(int row, int col) {
-            this.row = row;
-            this.col = col;
+        public Node(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
 
-        public Node(int row, int col, int cnt) {
-            this.row = row;
-            this.col = col;
+        public Node(int r, int c, int cnt) {
+            this.r = r;
+            this.c = c;
             this.cnt = cnt;
         }
     }
