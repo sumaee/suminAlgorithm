84dec06894ac5d30f44f275aeb34f8a3e584d180
sumaee <109265165+sumaee@users.noreply.github.com>
2025-10-03 17:56:24 +0900

    [Gold V] Title: 퇴사 2, Time: 700 ms, Memory: 330804 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,7 +1,6 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,24 +9,20 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         Counsel[] counsels = new Counsel[n + 1];
+
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             int time = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            counsels[i] = new Counsel(time, cost);
+            int price = Integer.parseInt(st.nextToken());
+            counsels[i] = new Counsel(time, price);
         }
 
         int[] dp = new int[n + 2];
         for (int i = n; i > 0; i--) {
-            //날짜가 초과된다면
             if (i + counsels[i].time > n + 1) {
                 dp[i] = dp[i + 1];
-            }
-            //날짜가 초과되지 않는다면
-            else {
-                //다음 상담을 진행할 수 있는 dp의 값과 이전까지의 값과 비교해서 더 큰값을 넣어줌
-                dp[i] = Math.max(dp[i + 1], counsels[i].cost + dp[i + counsels[i].time]);
+            } else {
+                dp[i] = Math.max(dp[i + 1], counsels[i].price + dp[i + counsels[i].time]);
             }
         }
 
@@ -35,11 +30,11 @@ public class Main {
     }
 
     static class Counsel {
-        int time, cost;
+        int time, price;
 
-        public Counsel(int time, int cost) {
+        public Counsel(int time, int price) {
             this.time = time;
-            this.cost = cost;
+            this.price = price;
         }
     }
 }
